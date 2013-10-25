f6f412f0bec94bc4785f8041436e876531ed656b
Ethan Langevin <ethan_langevin@brown.edu>
2013-10-24 22:36:20 -0400

    Added mutex to prevent concurrent resource definition loads

--- spec/lib/attributes_spec.rb
+++ spec/lib/attributes_spec.rb
@@ -7,7 +7,7 @@ describe "Attributes" do
   before(:all) do
     TestResource.reload_class_attributes
   end
-  
+
   after(:all) do
     TestResource.reload_class_attributes
   end
@@ -23,7 +23,7 @@ describe "Attributes" do
     it "should not allow mass assignment of protected attributes" do
       test_resource = TestResource.new
       lambda{
-        test_resource.attributes = {:protected_attr => 100}  
+        test_resource.attributes = {:protected_attr => 100}
       }.should raise_error
     end
 
@@ -42,7 +42,7 @@ describe "Attributes" do
       TestResource.attribute?(:attr1).should be_true
       TestResource.attribute?(:attr2).should be_true
     end
-    
+
 
     describe "Determining Attributes, Scopes, and Associations from the server" do
 
@@ -51,12 +51,12 @@ describe "Attributes" do
         tst.attribute?(:name).should be_true
         tst.attribute?(:age).should be_true
       end
-      
+
       it "should typecast data if a format is specified" do
         tst = TestResource.new(:bday => Date.today.to_s)
         tst.bday.should be_a Date
       end
-      
+
       it "should typecast data if a format is specified" do
         tst = TestResource.new(:roles => [:role1, :role2])
         tst.roles.should be_a Array
@@ -72,7 +72,7 @@ describe "Attributes" do
         tst.association?(:has_many_objects).should be_true
         tst.association?(:belongs_to_object).should be_true
       end
-      
+
       it "should be able to determine scopes when the class loads" do
         tst = TestResource.new
         tst.scope?(:paginate).should be_true
@@ -94,7 +94,7 @@ describe "Attributes" do
         TestResource.define_attributes :attr1, :attr2
         TestResource.define_protected_attributes :attr3
       end
-    
+
       it "should set attributes for the data loaded from a hash" do
         tst = TestResource.new({:attr1 => "attr1", :attr2 => "attr2"})
         tst.attr1?.should be_true
@@ -102,9 +102,9 @@ describe "Attributes" do
         tst.attr1 = "test"
         tst.attr1.should eql("test")
       end
-    
+
       it "should create protected attributes for unknown attributes trying to be loaded" do
-        
+
         TestResource.connection.stubs(:get => {:attr1 => "attr1", :attr3 => "attr3"})
         tst = TestResource.find(1)
 
@@ -120,7 +120,7 @@ describe "Attributes" do
       tst.respond_to?(:attr1=).should be_true
       tst.respond_to?(:attr1?).should be_true
     end
-    
+
     it "should be able to set and change attributes" do
       TestResource.define_attributes :attr1, :attr2
       tst = TestResource.new
@@ -130,20 +130,20 @@ describe "Attributes" do
       tst.attr1.should eql("test")
       tst.attr1?.should be_true
     end
-    
+
     it "should be able to set multiple attributes at once" do
       TestResource.define_attributes :attr1, :attr2, :attr3
       tst = TestResource.new
       tst.attr3 = "123"
-      
+
       tst.attributes = {:attr1 => "abc", :attr2 => "test"}
       tst.attr1.should eql "abc"
       tst.attr2.should eql "test"
       tst.attr3.should eql "123"
     end
-    
+
   end
-  
+
   context "Dirty tracking" do
     context "Changes to attributes" do
       it "should implement dirty tracking for attributes" do
@@ -153,20 +153,20 @@ describe "Attributes" do
         tst.attr1 = "Hello"
         tst.changed.include?("attr1").should be_true
         tst.changes.should_not be_blank
-        
+
         # Set an attribute equal to itself
         tst.attr2 = tst.attr2
         tst.changes.include?("attr2").should be_false
       end
 
     end
-    
+
     context "Resetting and marking attributes current" do
-      
+
       before(:each) do
         TestResource.define_attributes :attr1, :attr2
       end
-      
+
       it "should be able to mark any list of attributes as current (unchanged)" do
         tst = TestResource.new
         tst.attr1 = "Hello"
@@ -174,7 +174,7 @@ describe "Attributes" do
         tst.set_attributes_as_current :attr1, :attr2
         tst.changed.should be_blank
       end
-      
+
       it "should be able to mark all the attributes as current if none are given" do
         tst = TestResource.new
         tst.attr1 = "attr1"
@@ -183,7 +183,7 @@ describe "Attributes" do
         tst.set_attributes_as_current
         tst.changed.should be_blank
       end
-      
+
       it "should be able to reset any list of attributes" do
         tst = TestResource.new
         tst.attr1 = "attr1"
@@ -191,7 +191,7 @@ describe "Attributes" do
         tst.attr1.should be_nil
         tst.changed.should be_blank
       end
-      
+
       it "should be able to reset all the attributes if none are given" do
         tst = TestResource.new
         tst.attr1 = "attr1"
@@ -203,7 +203,7 @@ describe "Attributes" do
         tst.changed.should be_blank
       end
     end
-    
+
   end
-  
+
 end
